c46ba3e3ab5dfd58e7d84c343d63e89a22c1530c
Giovanni Bassi <giggio@giggio.net>
2012-11-29 12:58:27 -0200

    refatorando o grupo de multas pra encapsular melhor os dados

--- src/MultasSociais/MultasSociais.Lib/Models/GrupoDeMultas.cs
+++ src/MultasSociais/MultasSociais.Lib/Models/GrupoDeMultas.cs
@@ -4,8 +4,26 @@ namespace MultasSociais.Lib.Models
 {
     public class GrupoDeMultas
     {
-        public IEnumerable<Multa> Itens { get; set; }
-        public string Nome { get; set; }
-        public TipoGrupo TipoGrupo { get; set; }
+        private readonly List<Multa> itens = new List<Multa>();
+
+        public GrupoDeMultas(string nome, TipoGrupo tipoGrupo)
+        {
+            Nome = nome;
+            TipoGrupo = tipoGrupo;
+        }
+        public void Add(IEnumerable<Multa> multas)
+        {
+            itens.AddRange(multas);
+            foreach (var multa in multas)
+                multa.Grupo = this;
+        }
+        public void Add(params Multa[] multas)
+        {
+            Add((IEnumerable<Multa>) multas);
+        }
+
+        public IEnumerable<Multa> Itens { get { return itens; } }
+        public string Nome { get; private set; }
+        public TipoGrupo TipoGrupo { get; private set; }
     }
 }
--- src/MultasSociais/MultasSociais.Lib/Models/Talao.cs
+++ src/MultasSociais/MultasSociais.Lib/Models/Talao.cs
@@ -23,11 +23,8 @@ namespace MultasSociais.Lib.Models
            var request = WebRequest.CreateHttp("http://multassociais.net/multas.json");
            var responseContent = await request.GetResponseAsync();
            var multas = JsonConvert.DeserializeObject<IEnumerable<Multa>>(responseContent);
-           var grupo = new GrupoDeMultas {Itens = multas, TipoGrupo = TipoGrupo.MaisNovos, Nome = "Mais novos"};
-           foreach (var multa in multas)
-           {
-               multa.Grupo = grupo;
-           } 
+           var grupo = new GrupoDeMultas("Mais novos",TipoGrupo.MaisNovos);
+           grupo.Add(multas);
            return grupo;
        }
        public async Task<GrupoDeMultas> ObterMaisMultados()
@@ -35,11 +32,8 @@ namespace MultasSociais.Lib.Models
            var request = WebRequest.CreateHttp("http://multassociais.net/multas.json");
            var responseContent = await request.GetResponseAsync();
            var multas = JsonConvert.DeserializeObject<IEnumerable<Multa>>(responseContent);
-           var grupo = new GrupoDeMultas { Itens = multas, TipoGrupo = TipoGrupo.MaisMultados, Nome = "Mais multados" };
-           foreach (var multa in multas)
-           {
-               multa.Grupo = grupo;
-           } 
+           var grupo = new GrupoDeMultas ("Mais multados", TipoGrupo.MaisMultados);
+           grupo.Add(multas);
            return grupo;
        }
 
@@ -64,26 +58,16 @@ namespace MultasSociais.Lib.Models
         public static GrupoDeMultas MaisMultados { get; set; }
         static Talao()
         {
-            MaisNovos = new GrupoDeMultas
-                            {
-                                Nome = "Mais novos",
-                                TipoGrupo = TipoGrupo.MaisNovos
-                            };
-            MaisNovos.Itens = new List<Multa>
-                      {
+            MaisNovos = new GrupoDeMultas("Mais novos", TipoGrupo.MaisNovos);
+            MaisNovos.Add(
                           new Multa{ Id = 1, DataOcorrencia = new DateTime(2012,1,2), NumeroDeMultas = 3, Placa = "ABC 1234", VideoUrl = "", FotoUrl = "http://s3.amazonaws.com/msociais/fotos/15/original/fotoblur.png?1324341452", Grupo = MaisNovos, Descricao = "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."},
-                          new Multa{ Id = 2, DataOcorrencia = new DateTime(2012,3,4), NumeroDeMultas = 5, Placa = "DEF 1234", VideoUrl = "", FotoUrl = "http://s3.amazonaws.com/msociais/fotos/14/original/IMG_8539.JPG?1324307845", Grupo = MaisNovos, Descricao = "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa."},
-                      };
-            MaisMultados = new GrupoDeMultas
-                               {
-                                   Nome = "Mais multados",
-                                   TipoGrupo = TipoGrupo.MaisMultados
-                               };
-            MaisMultados.Itens = new List<Multa>
-                      {
+                          new Multa{ Id = 2, DataOcorrencia = new DateTime(2012,3,4), NumeroDeMultas = 5, Placa = "DEF 1234", VideoUrl = "", FotoUrl = "http://s3.amazonaws.com/msociais/fotos/14/original/IMG_8539.JPG?1324307845", Grupo = MaisNovos, Descricao = "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa."}
+                      );
+            MaisMultados = new GrupoDeMultas("Mais multados", TipoGrupo.MaisMultados);
+            MaisMultados.Add(
                           new Multa{ Id = 3, DataOcorrencia = new DateTime(2012,1,2), NumeroDeMultas = 3, Placa = "ABC 1234", VideoUrl = "", FotoUrl = "http://s3.amazonaws.com/msociais/fotos/15/original/fotoblur.png?1324341452", Grupo = MaisMultados, Descricao = "Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis."},
-                          new Multa{ Id = 4, DataOcorrencia = new DateTime(2012,3,4), NumeroDeMultas = 5, Placa = "DEF 1234", VideoUrl = "", FotoUrl = "http://s3.amazonaws.com/msociais/fotos/14/original/IMG_8539.JPG?1324307845", Grupo = MaisMultados, Descricao = "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa."},
-                      };
+                          new Multa{ Id = 4, DataOcorrencia = new DateTime(2012,3,4), NumeroDeMultas = 5, Placa = "DEF 1234", VideoUrl = "", FotoUrl = "http://s3.amazonaws.com/msociais/fotos/14/original/IMG_8539.JPG?1324307845", Grupo = MaisMultados, Descricao = "Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa."}
+                      );
         }
 #endif
     }
