44ccc647f5e0ab976298e18c7b82667f68c5bdca
Giovanni Bassi <giggio@giggio.net>
2012-12-06 19:11:26 -0200

    transformando os campos em controles

--- src/MultasSociais/MultasSociais.WinStoreApp/ViewModels/MultaNovaViewModel.cs
+++ src/MultasSociais/MultasSociais.WinStoreApp/ViewModels/MultaNovaViewModel.cs
@@ -1,11 +1,28 @@
-﻿using Caliburn.Micro;
+﻿using System.Threading.Tasks;
+using Caliburn.Micro;
 using MultasSociais.Lib.Models;
 
 namespace MultasSociais.WinStoreApp.ViewModels
 {
     public class MultaNovaViewModel : ViewModelBase
     {
+        private bool multando;
         public MultaNovaViewModel(INavigationService navigationService, ITalao talao) : base(navigationService, talao) {}
 
+        public async Task Multar()
+        {
+            Multando = true;
+            await Task.Delay(5000);
+        }
+
+        public bool Multando
+        {
+            get { return multando; }
+            set
+            {
+                multando = value;
+                NotifyOfPropertyChange();
+            }
+        }
     }
 }
