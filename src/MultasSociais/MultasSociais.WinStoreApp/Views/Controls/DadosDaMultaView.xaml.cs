1f2f3718c208484e53e449469f1921303b4a2a8a
Giovanni Bassi <giggio@giggio.net>
2012-12-08 23:39:01 -0200

    acrescentando o .xaml.cs ao controle, parece que não funciona sem ele.

---
+++ src/MultasSociais/MultasSociais.WinStoreApp/Views/Controls/DadosDaMultaView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace MultasSociais.WinStoreApp.Views.Controls
+{
+    public sealed partial class DadosDaMultaView : UserControl
+    {
+        public DadosDaMultaView()
+        {
+            InitializeComponent();
+        }
+    }
+}
