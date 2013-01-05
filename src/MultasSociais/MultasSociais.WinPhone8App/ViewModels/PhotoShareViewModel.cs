13cb72da1298a0fd8a5637c280da05065d7e635a
Giovanni Bassi <giggio@giggio.net>
2013-01-05 21:03:55 -0200

    colocando imagem na pagina do share e no viewmodel

--- src/MultasSociais/MultasSociais.WinPhone8App/ViewModels/PhotoShareViewModel.cs
+++ src/MultasSociais/MultasSociais.WinPhone8App/ViewModels/PhotoShareViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MultasSociais.Lib.Models;
+﻿using System.Windows.Media.Imaging;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Media.PhoneExtensions;
+using MultasSociais.Lib.Models;
 
 namespace MultasSociais.WinPhone8App.ViewModels
 {
@@ -6,6 +9,7 @@ namespace MultasSociais.WinPhone8App.ViewModels
     {
         private readonly ITalao talao;
         private string fileId;
+        private BitmapImage imagem;
 
         public PhotoShareViewModel(ITalao talao)
         {
@@ -19,6 +23,28 @@ namespace MultasSociais.WinPhone8App.ViewModels
             {
                 if (value == fileId) return;
                 fileId = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        protected override void OnActivate()
+        {
+            var library = new MediaLibrary();
+            var photoFromLibrary = library.GetPictureFromToken(FileId);
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.SetSource(photoFromLibrary.GetImage());
+            Imagem = bitmapImage;
+            base.OnActivate();
+        }
+
+        public BitmapImage Imagem
+        {
+            get { return imagem; }
+            set
+            {
+                if (value == imagem) return;
+                imagem = value; 
                 NotifyOfPropertyChange();
             }
         }
