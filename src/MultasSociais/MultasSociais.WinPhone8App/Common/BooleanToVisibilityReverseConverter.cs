bac2cc0d446c4a44bd2c5ef31745708465c983bf
Giovanni Bassi <giggio@giggio.net>
2013-03-01 12:00:31 -0300

    mostrando progressbar durante carregamento

---
+++ src/MultasSociais/MultasSociais.WinPhone8App/Common/BooleanToVisibilityReverseConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MultasSociais.WinPhone8App.Common
+{
+    public class BooleanToVisibilityReverseConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Collapsed;
+        }
+    }
+}
